b91d4f48a5b3cfeee7c6a79d684e66f236dfb20c
Georg Graf (SPG) <grafg@spengergasse.at>
2023-09-02 21:38:16 +0200

    prettier went over it!

--- models/uhren.js
+++ models/uhren.js
@@ -1,24 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const uhrenSchema = new mongoose.Schema({
     user: {
         type: String,
-        required: true
+        required: true,
     },
     uhr: {
         type: String,
-        required: true
+        required: true,
     },
     dateMeasured: {
         type: Date,
-        required: true
+        required: true,
     },
     offsetSecs: {
         type: Number,
         required: true,
-        default: 0
-    }
-})
+        default: 0,
+    },
+});
 
-module.exports = mongoose.model('Uhren', uhrenSchema);
-
+module.exports = mongoose.model("Uhren", uhrenSchema);
