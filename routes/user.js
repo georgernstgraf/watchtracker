4bd399ca39292081ba8ecd68bedb38efe36f3692
Georg Graf (SPG) <grafg@spengergasse.at>
2024-05-30 13:55:35 +0200

    finish #42

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const TimeZone = require('../classes/timeZone');
 const User = require('../classes/user');
+const Watch = require('../classes/watch');
 router.patch('', async (req, res) => {
     const userObj = new User(req.session.user);
     userObj.patch(req.body);
     const user = await userObj.save();
     req.session.user = user.getCurrentData();  // not daring / willing to put a proxy object onto the session
-    return res.render('body', { user, timeZones: TimeZone.timeZones });
+    const userWatches = await Watch.userWatches(user);
+    const watch = await Watch.userWatchWithMeasurements(user);
+    return res.render('body', { user, timeZones: TimeZone.timeZones, userWatches, watch });
 });
 module.exports = router;
