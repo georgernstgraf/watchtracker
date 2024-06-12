e0bf1e1b513807dd0eed33c76de1d2df9a8a46c3
Georg Graf (SPG) <grafg@spengergasse.at>
2024-06-12 18:09:07 +0200

    timezone fixup

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,10 @@ const User = require('../classes/user');
 const Watch = require('../classes/watch');
 router.patch('', async (req, res) => {
     const userObj = new User(req.session.user);
-    if (req.body.timeZone && !TimeZone.timeZones.includes(req.body.timeZone)) {
-        return res.status(422).send(e.message);
+    if (req.body.hasOwnProperty('timeZone') && !TimeZone.timeZones.includes(req.body.timeZone)) {
+        return res.status(422).send("Unknown / invalid time zone");
     }
+    userObj.patch(req.body);
     const user = await userObj.save();
     req.session.user = user.getCurrentData();  // not daring / willing to put a proxy object onto the session
     const userWatches = await Watch.userWatches(user);
