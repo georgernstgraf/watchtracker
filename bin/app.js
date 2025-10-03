94136d0e41d0998766f0fb45d6ed184c9330256f
Georg Graf (SPG) <grafg@spengergasse.at>
2025-10-03 06:37:53 +0200

    in the midst of insanity

--- bin/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-// the following require loads and executes the default export from the modules' main config in package.json
-require('../main')({
-    host: process.env.APP_HOST || 'localhost',
-    port: process.env.APP_PORT || 8000,
-});
---
+++ lib/session.ts
@@ -0,0 +1,28 @@
+const ms = require("ms");
+const expressSession = require("express-session");
+const MemcachedStore = require("connect-memcached")(expressSession);
+ms(process.env.COOKIE_MAX_AGE) / 1000;
+export const session = expressSession({
+    name: process.env.COOKIE_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    proxy: process.env.NODE_ENV === "production",
+    cookie: {
+        maxAge: ms(process.env.COOKIE_MAX_AGE),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        path: process.env.APP_PATH,
+    },
+    store: new MemcachedStore({
+        hosts: ["127.0.0.1:11211"],
+    }),
+});
+export const logoutCookie = {
+    maxAge: 0,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    path: process.env.APP_PATH,
+};
