b5dddba170b28b634268e7b97591d6930a75ef0d
Georg Graf (SPG) <grafg@spengergasse.at>
2025-10-03 09:20:31 +0200

    more stuff

--- routes/login.ts
+++ routes/login.ts
@@ -6,45 +6,45 @@ import TimeZone from "../classes/timeZone.ts";
 // this gets the login form req.body.passwd, req.body.user
 // renders the index page on success
 router.post("/", async (req, res) => {
-    const errors = (res.locals.errors = []);
-    if (!req.body.user || req.body.user.trim() === "") {
-        errors.push("Username is required");
-    }
-    if (!req.body.passwd || req.body.passwd.trim() === "") {
-        errors.push("Password is required");
-    }
-    if (errors.length !== 0) {
-        return res.render("body-login");
-    }
-    const userName = req.body.user;
-    const passwd = req.body.passwd;
-    try {
-        const authResp = await fetch(process.env.AUTH_API_URL, {
-            method: "POST",
-            body: JSON.stringify({
-                user: userName,
-                passwd: passwd,
-            }),
-            headers: { "Content-Type": "application/json" },
-        });
-        // auth (bool) und user (string)
-        if (!(await authResp.json()).auth) {
-            throw new Error("invalid credentials");
-        }
-    } catch (err) {
-        errors.push(`login failed: ${err.message}`);
-        return res.render("body-login");
-    }
-    // registers the session and sends the cookie
-    const user = await User.enforceExists(userName);
-    req.session.user = user.getCurrentData();
-    const userWatches = await Watch.userWatches(user);
-    const watch = await Watch.userWatchWithMeasurements(user);
-    return res.render("body-auth", {
-        user: req.session.user,
-        userWatches,
-        watch,
-        timeZones: TimeZone.timeZones,
+  const errors = (res.locals.errors = []);
+  if (!req.body.user || req.body.user.trim() === "") {
+    errors.push("Username is required");
+  }
+  if (!req.body.passwd || req.body.passwd.trim() === "") {
+    errors.push("Password is required");
+  }
+  if (errors.length !== 0) {
+    return res.render("login-body");
+  }
+  const userName = req.body.user;
+  const passwd = req.body.passwd;
+  try {
+    const authResp = await fetch(process.env.AUTH_API_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        user: userName,
+        passwd: passwd,
+      }),
+      headers: { "Content-Type": "application/json" },
     });
+    // auth (bool) und user (string)
+    if (!(await authResp.json()).auth) {
+      throw new Error("invalid credentials");
+    }
+  } catch (err) {
+    errors.push(`login failed: ${err.message}`);
+    return res.render("login-body");
+  }
+  // registers the session and sends the cookie
+  const user = await User.enforceExists(userName);
+  req.session.user = user.getCurrentData();
+  const userWatches = await Watch.userWatches(user);
+  const watch = await Watch.userWatchWithMeasurements(user);
+  return res.render("body-auth", {
+    user: req.session.user,
+    userWatches,
+    watch,
+    timeZones: TimeZone.timeZones,
+  });
 });
 export default router;
--- routes/logout.ts
+++ routes/logout.ts
@@ -4,11 +4,11 @@ import { logoutCookie } from "../lib/session.ts";
 const router = Router();
 // this gets the login form req.body.passwd, req.body.user
 router.post("/", async (req, res) => {
-    req.session.destroy();
-    res.cookie(process.env.COOKIE_NAME, "logout", logoutCookie);
-    if (req.headers["hx-request"]) {
-        return res.render("body-login");
-    }
-    return res.render("login-full");
+  req.session.destroy();
+  res.cookie(process.env.COOKIE_NAME, "logout", logoutCookie);
+  if (req.headers["hx-request"]) {
+    return res.render("login-body");
+  }
+  return res.render("login-full");
 });
 export default router;
