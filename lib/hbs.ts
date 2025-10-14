443939eb3b30a46f4d7ed99f7be7cf72d83aa394
Georg Graf (SPG) <grafg@spengergasse.at>
2025-10-14 06:20:50 +0200

    #56 render function not stored on session objects

--- lib/hbs.ts
+++ lib/hbs.ts
@@ -3,21 +3,11 @@ import { walk } from "@std/fs";
 import { partialsDir } from "./config.ts";
 import { renderData } from "./types.ts";
 
-export const map = new Map();
+// map "template name" => compiled template function
+const map = new Map();
 
 export function render(templateName: string, renderData: renderData): string {
     return map.get(templateName)(renderData);
-}
-
-async function listHBSFilesRecursive(dir: string): Promise<string[]> {
-    const dirEntries: string[] = [];
-    for await (const entry of walk(dir, { exts: [".hbs"], includeDirs: false })) {
-        // entry: { path, name, isFile, isDirectory, isSymlink }
-        if (entry.isFile) {
-            dirEntries.push(entry.path);
-        }
-    }
-    return dirEntries;
 }
 
 async function loadTemplates(): Promise<void> {
@@ -32,7 +22,17 @@ async function loadTemplates(): Promise<void> {
     }
 }
 
-// Register hbs helpers
+async function listHBSFilesRecursive(dir: string): Promise<string[]> {
+    const dirEntries: string[] = [];
+    for await (const entry of walk(dir, { exts: [".hbs"], includeDirs: false })) {
+        // entry: { path, name, isFile, isDirectory, isSymlink }
+        if (entry.isFile) {
+            dirEntries.push(entry.path);
+        }
+    }
+    return dirEntries;
+}
+
 hbs.registerHelper("let", function (this: unknown, options: hbs.HelperOptions) {
     const context = Object.assign({}, this, options.hash);
     return options.fn(context);
@@ -77,10 +77,11 @@ hbs.registerHelper("plusOne", function (val: number) {
     if (val == 0) return 1;
     return val;
 });
+
 try {
     await loadTemplates();
-    console.log("Templates loaded successfully:", [...map.keys()]);
+    console.log("HBS: Templates loaded successfully:", [...map.keys()]);
 } catch (err) {
-    console.error("Error loading templates:", err);
+    console.error("HBS: Error loading templates:", err);
     Deno.exit(1);
 }
--- routers/sessionRouter.ts
+++ routers/sessionRouter.ts
@@ -1,13 +1,12 @@
 import { Hono } from "hono";
 
 import { Session } from "../middleware/session.ts";
-import { render } from "../lib/hbs.ts";
 
 import * as slash from "../routes/slash.ts";
 import * as login from "../routes/login.ts";
 import * as logout from "../routes/logout.ts";
 
-export const sessionRouter = new Hono<{ Variables: { session: Session; render: typeof render; errors: string[] } }>();
+export const sessionRouter = new Hono<{ Variables: { session: Session } }>();
 sessionRouter.use(async (c, next) => {
     return await Session.middleware(c, next);
 });
