09aa39d64de981e569632fea361410a1ac8c25a7
Georg Graf (SPG) <grafg@spengergasse.at>
2026-02-10 23:29:02 +0100

    Remove outdated service/README.md

--- bastelzeug/debug_env.ts
+++
@@ -1,9 +0,0 @@
-import * as config from "../lib/config.ts";
-
-// Debug environment variables
-console.log("Environment variables:");
-console.log("COOKIE_NAME:", config.COOKIE_NAME);
-console.log("COOKIE_SECRET:", config.COOKIE_SECRET);
-console.log("COOKIE_MAX_AGE:", config.COOKIE_MAX_AGE);
-console.log("APP_PATH:", config.APP_PATH);
-console.log("DENO_ENV:", config.DENO_ENV);
--- bastelzeug/deno-sqlite.ts
+++
@@ -1,5 +0,0 @@
-import { Database } from "@db/sqlite";
-const db = new Database(":memory:");
-const [version] = db.prepare("select sqlite_version()").value<[string]>()!;
-console.log(version);
-db.close();
