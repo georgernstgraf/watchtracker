b7d2fd314695ab4e882c5ae625027bbb214735d2
Georg Graf (SPG) <grafg@spengergasse.at>
2026-02-12 19:15:54 +0100

    small fixes

--- scripts/replacedb.sh
+++ scripts/replacedb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -v
-fuser watchtracker.db >/dev/null 2>&1 && echo 'DB IN USE' || (rm watchtracker.db* && cp vacuum.db watchtracker.db)
+fuser watchtracker.db >/dev/null 2>&1 && echo 'DB IN USE' || (rm -f watchtracker.db* && cp vacuum.db watchtracker.db)
